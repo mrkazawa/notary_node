f1c5901afd0458b4dffb8f77a3eff587cf873670
Yustus Oktian <yustus.oktian@gmail.com>
2020-02-05 18:04:14 +0900

    remove web worker because cannot increase the performance after testing

--- src/benchmark/broadcast_tx/web_worker/service.js
+++
@@ -1,5 +0,0 @@
-const { workerData, parentPort } = require('worker_threads')
-
-// You can do any heavy stuff here, in a synchronous way
-// without blocking the "main thread"
-parentPort.postMessage({ hello: workerData })
