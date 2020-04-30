592600623d17034d752601f194cda4d81721653a
Yustus Oktian <yustus.oktian@gmail.com>
2020-04-30 11:03:38 +0900

    add test cases for iota payment engine

--- src/payment/iota_engine.js
+++ src/payment/iota_engine.js
@@ -1,21 +1,22 @@
-const IOTA = require('@iota/core');
+const Iota = require('@iota/core');
+const Converter = require('@iota/converter');
+const Extract = require('@iota/extract-json');
+
+// specify the location of the IRI node
+const iota = Iota.composeAPI({
+  provider: 'http://localhost:14265'
+});
 
 const DEPTH = 3;
 const MINIMUM_WEIGHT_MAGNITUDE = 9;
 const SECURITY_LEVEL = 0;
-
 const SEED = 'SENDER99999999999999999999999999999999999999999999999999999999999999999999999999A';
-
-// specify the location of the IRI node
-iota = IOTA.composeAPI({
-  provider: 'http://localhost:14265'
-});
 
 // TODO: Implement the generate address and attach to Tangle to correct way
 
 var self = {
   generateNextAddress: function () {
-    return IOTA.generateAddress(SEED, SECURITY_LEVEL);
+    return Iota.generateAddress(SEED, SECURITY_LEVEL);
   },
 
   attachToTangle: async function (address) {
@@ -43,6 +44,85 @@ var self = {
 }
 
 var iota_engine = {
+  getNodeInfo: async function () {
+    try {
+      return await iota.getNodeInfo();
+    } catch (err) {
+      console.log(`Error when getting node info: ${err}`);
+    }
+  },
+
+  convertAsciiToTrytes: function (message) {
+    return Converter.asciiToTrytes(message);
+  },
+
+  sendTx: async function (transfers) {
+    try {
+      const trytes = await iota.prepareTransfers(SEED, transfers);
+      const bundle = await iota.sendTrytes(trytes, DEPTH, MINIMUM_WEIGHT_MAGNITUDE);
+      const tailTxHash = bundle[0].hash;
+
+      return tailTxHash;
+    } catch (err) {
+      console.log(`Error when sending Tx: ${err}`);
+    }
+  },
+
+  readTxMessage: async function (tailTxHash) {
+    try {
+      const bundle = await iota.getBundle(tailTxHash);
+      return JSON.parse(Extract.extractJson(bundle));
+    } catch (err) {
+      console.log(`Error when reading Tx: ${err}`);
+    }
+  },
+
+  generateAddress: async function () {
+    try {
+      return await iota.getNewAddress(SEED, {
+        index: 0,
+        securityLevel: SECURITY_LEVEL,
+        total: 1
+      });
+    } catch (err) {
+      console.log(`Error when generating new address: ${err}`);
+    }
+  },
+
+  isTxVerified: async function (tailTxHash) {
+    try {
+      return await iota.getLatestInclusion([tailTxHash]);
+    } catch (err) {
+      console.log(`Error when checking tx: ${err}`);
+    }
+  },
+
+  getBalances: async function (address) {
+    try {
+      const balance = await iota.getBalances([address], 100);
+      return parseInt(balance.balances);
+    } catch (err) {
+      console.log(`Error when checking balances: ${err}`);
+    }
+  },
+
+  createRandomIotaTag: function () {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
+    const charactersLength = characters.length;
+    const length = 27; // IOTA tag length is 27 trytes
+
+    var result = '';
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  },
+
+
+
+
+
   /**
    * Get the current balance for given address in the Tangle (IOTA Network).
    * 
@@ -126,22 +206,6 @@ var iota_engine = {
     }
 
     return false;
-  },
-
-  /**
-   * Create a random IOTA tag for transactions.
-   */
-  createRandomIotaTag: function () {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
-    const charactersLength = characters.length;
-    const length = 27; // IOTA tag length is 27 trytes
-
-    var result = '';
-    for (var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-
-    return result;
   }
 }
 
---
+++ src/payment/tests/iota.test.js
@@ -0,0 +1,65 @@
+const paymentEngine = require('../iota_engine');
+
+const address = 'HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWOR99D';
+jest.setTimeout(120000);
+
+test('Getting node info', async () => {
+  const nodeIndo = await paymentEngine.getNodeInfo();
+  // In JavaScript, there are six falsy values:
+  // false, 0, '', null, undefined, and NaN.
+  // Everything else is truthy.
+  expect(nodeIndo).toBeTruthy();
+});
+
+test('Sending and reading Hello World transaction', async () => {
+  const message = JSON.stringify({
+    "message": "Hello world"
+  });
+  const messageInTrytes = paymentEngine.convertAsciiToTrytes(message);
+
+  const transfers = [{
+    value: 0,
+    address: address,
+    message: messageInTrytes
+  }];
+
+  const tailTxHash = await paymentEngine.sendTx(transfers);
+  expect(tailTxHash).toBeTruthy();
+
+  const queriedMessage = await paymentEngine.readTxMessage(tailTxHash);
+  expect(queriedMessage).toEqual({
+    "message": "Hello world"
+  });
+});
+
+test('Generating new address', async () => {
+  const address = await paymentEngine.generateAddress();
+  expect(address).toBeTruthy();
+});
+
+test('Sending iota in a transaction', async () => {
+  const transfers = [{
+    value: 1,
+    address: address,
+    tag: paymentEngine.createRandomIotaTag()
+  }];
+
+  const beforeBalance = await paymentEngine.getBalances(address);
+  expect(beforeBalance).toBeTruthy();
+
+  const tailTxHash = await paymentEngine.sendTx(transfers);
+  expect(tailTxHash).toBeTruthy();
+
+  while (true) {
+    // waiting until the payment is verified
+    // it can take 60 seconds, depending on the `tick` parameter in
+    // the COO node.
+
+    const confirmed = await paymentEngine.isTxVerified(tailTxHash);
+    if (confirmed[0]) {
+      const afterBalance = await paymentEngine.getBalances(address);
+      expect(afterBalance).toEqual(beforeBalance + transfers[0].value);
+      break;
+    }
+  }
+});
